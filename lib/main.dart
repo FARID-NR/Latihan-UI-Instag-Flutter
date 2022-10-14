428392e61dac1ac377140ca11aa49743ed57c2ab
FARID-NR <nurfarid776@gmail.com>
2022-10-14 22:48:43 +0800

    Pembuatan AppBar (Progress 2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'icons_custom/instag_icons.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -13,6 +14,18 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset('gambar/logo1.png', ),
+              IconButton(onPressed: (() {}), icon:Icon(Icons.expand_more_outlined), color: Colors.black, iconSize: 25, padding: EdgeInsets.zero, splashRadius: 15, )
+            ],
+          ),
+          actions: [
+            IconButton(onPressed: (() {}), icon: Icon(Icons.add_box_outlined), color: Colors.black, splashRadius: 25),
+            IconButton(onPressed: (() {}), icon: Icon(Instag.heart), color: Colors.black, splashRadius: 25),
+            IconButton(onPressed: (() {}), icon: Icon(Instag.share), color: Colors.black, splashRadius: 25,)
+          ],
         ),
       ),
     );
